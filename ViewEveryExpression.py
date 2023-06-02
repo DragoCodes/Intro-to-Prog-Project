d2d9cb6ec6becbb5f88c41ea3d5d4c2f467e9dc1
sidarthprabhu <133030311+sidarthprabhu@users.noreply.github.com>
2023-06-02 10:17:39 +0530

    Update ViewEveryExpression.py
    Comments

--- ViewEveryExpression.py
+++ ViewEveryExpression.py
@@ -15,9 +15,11 @@ plt.figure(0, figsize = (12,20))
 cpt = 0
 
 for emotion in os.listdir(path + "train"):
+	#Taking 5 images of each Emotion
 	for i in range(5):
 		cpt=cpt+1
 		plt.subplot(7,5,cpt)
 		img = load_img(path + "train/" + emotion + "/" +os.listdir(path + "train/" + emotion)[i], target_size=(photosize, photosize)))
 		plt.imshow(img, cmap = "gray")
+#To show the images using matplotlib
 plt.show()
