962c8e281f05210644d1daa4035e88b0ac0dcaa8
Drago <134218117+DragoCodes@users.noreply.github.com>
2023-06-03 19:23:30 +0530

    Update FaceDontLie.py

--- FaceDontLie.py
+++ FaceDontLie.py
@@ -8,8 +8,9 @@ import copy
 
 text_list = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 newmodel = load_model('model_weights.h5')
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +
                                      'haarcascade_frontalface_default.xml')
+
 
 class Ui_MainWindow(object):
     def imagedetect(self):
@@ -27,7 +28,7 @@ class Ui_MainWindow(object):
                     print("Face not detected")
                 else:
                     for (ex, ey, ew, eh) in facess:
-                        where = ew/2
+                        where = ew / 2
                         face_roi = roi_color[ey:ey+eh, ex:ex+ew]
                         gray_image = cv2.cvtColor(face_roi, cv2.COLOR_BGR2GRAY)
                         resized_image = cv2.resize(gray_image, (56, 56))
@@ -35,9 +36,13 @@ class Ui_MainWindow(object):
                         input_data = input_data / 255.0
                         prediction = newmodel.predict(np.array([input_data]))
                         text_idx = np.argmax(prediction)
-                        cv2.putText(frame, text_list[text_idx], (x+int(where)-45, y-5), cv2.FONT_HERSHEY_SIMPLEX, 1.25, (255, 0, 255), 3)
+                        cv2.putText(frame, text_list[text_idx],
+                                    (x+int(where)-45, y-5),
+                                    cv2.FONT_HERSHEY_SIMPLEX,
+                                    1.25, (255, 0, 255), 3)
             plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             plt.show()
+
     def live(self):
         cap = cv2.VideoCapture(0)
         while True:
@@ -45,39 +50,42 @@ class Ui_MainWindow(object):
             img = copy.deepcopy(frame)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-            for x,y,w,h in faces:
+            for x, y, w, h in faces:
                 roi_gray = gray[y:y+h, x:x+w]
                 roi_color = frame[y:y+h, x:x+w]
-                cv2.rectangle(frame, (x,y), (x+w,y+h),(255,0,0),2)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
                 facess = face_cascade.detectMultiScale(roi_gray)
                 if len(facess) == 0:
                     print("Face not detected")
                 else:
-                    for (ex,ey,ew,eh) in facess:
+                    for (ex, ey, ew, eh) in facess:
                         face_roi = roi_color[ey:ey+eh, ex:ex+ew]
                         gray_image = cv2.cvtColor(face_roi, cv2.COLOR_BGR2GRAY)
                         resized_image = cv2.resize(gray_image, (56, 56))
                         input_data = np.expand_dims(resized_image, axis=-1)
-                        input_data = input_data / 255.0  # Normalize pixel values to [0, 1]
+                        input_data = input_data / 255.0
+                        # Normalize pixel values to [0, 1]
 
-                        # Pass the preprocessed input data to the model for prediction
+                        # Pass the preprocessed input data
+                        # to the model for prediction
                         prediction = newmodel.predict(np.array([input_data]))
                         text_idx = np.argmax(prediction)
-                
-                        text_list = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+
+                        text_list = ['Angry', 'Disgust', 'Fear',
+                                     'Happy', 'Neutral', 'Sad', 'Surprise']
                         text = text_list[text_idx]
                         cv2.putText(img, text, (x, y-5),
-                           cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 0, 255), 2)
-                        img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
-                    
-            
+                                    cv2.FONT_HERSHEY_SIMPLEX,
+                                    0.45, (255, 0, 255), 2)
+                        img = cv2.rectangle(img, (x, y), (x+w, y+h),
+                                            (0, 0, 255), 2)
             cv2.imshow("frame", img)
             key = cv2.waitKey(1)
             if key == 27:
                 break
-            
         cap.release()
         cv2.destroyAllWindows()
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(770, 441)
@@ -108,7 +116,8 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "Live Detection"))
-        self.pushButton_2.setText(_translate("MainWindow", "Browse for Pictures"))
+        self.pushButton_2.setText(_translate("MainWindow",
+                                             "Browse for Pictures"))
 
 
 if __name__ == "__main__":
